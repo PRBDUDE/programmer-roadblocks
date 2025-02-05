be1fbcf3322b5c18ccd0b00e0d8e509226ba6d37
PRBDUDE <programmerroadblocks@gmail.com>
2025-02-04 20:15:30 -0500

    create reactive entry form create route to reactive entry form add Validators

--- src/app/reactive-forms/reactive-entry-form/reactive-entry-form.component.ts
+++ src/app/reactive-forms/reactive-entry-form/reactive-entry-form.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-reactive-entry-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe
+  ],
   templateUrl: './reactive-entry-form.component.html',
   styleUrl: './reactive-entry-form.component.scss'
 })
 export class ReactiveEntryFormComponent {
+  validStateCodesPattern= /AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY/;
+  zipCodePattern = /^\d{5}(-\d{4})?$/;
+  phonePattern = /^\(?(\d{3})\)?[-. ]?(\d{3})[-. ]?(\d{4})$/;
 
+  entryForm: FormGroup = new FormGroup({
+    name: new FormGroup({
+      first: new FormControl('', Validators.required),
+      last: new FormControl('', Validators.required)
+    }),
+    address: new FormGroup({
+      street: new FormControl('', Validators.required),
+      city: new FormControl('', Validators.required),
+      state: new FormControl('', [Validators.required, Validators.pattern(this.validStateCodesPattern)]),
+      zip: new FormControl('', [Validators.required, Validators.pattern(this.zipCodePattern)]),
+    }),
+    contact: new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required, Validators.pattern(this.phonePattern)]),
+    })
+  } as any);
+
+  onSubmit() {
+    console.log(this.entryForm.value);
+  }
+
+  isValid() {
+    return !this.entryForm.valid;
+  }
 }
--- src/app/roadblock-header/roadblock-header.component.ts
+++ src/app/roadblock-header/roadblock-header.component.ts
@@ -164,6 +164,19 @@ export class RoadblockHeaderComponent implements OnInit {
                 }
               ]
             }
+          ],
+          [
+            {
+              label: 'Reactive Forms',
+              items: [
+                {
+                  label: 'Reactive Forms',
+                  subtext: 'Angular Reactive Forms guide',
+                  url: 'https://angular.dev/guide/forms/reactive-forms',
+                  target: '_blank'
+                }
+              ]
+            }
           ]
         ]
       }
