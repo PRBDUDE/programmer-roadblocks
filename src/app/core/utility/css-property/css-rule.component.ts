53c8414df93db3136af48588f417d2c3bd8e1cad
PRBDUDE <programmerroadblocks@gmail.com>
2025-02-18 12:40:00 -0500

    clean up formatting

--- src/app/core/utility/css-property/css-rule.component.ts
+++ src/app/core/utility/css-property/css-rule.component.ts
@@ -4,13 +4,13 @@ import {CssPropertyNameComponent} from "./css-property-name/css-property-name.co
 import {CssPropertyValueComponent} from "./css-property-value/css-property-value.component";
 
 @Component({
-    selector: 'app-css-rule',
+  selector: 'app-css-rule',
   imports: [
     CssPropertyNameComponent,
     CssPropertyValueComponent
   ],
-    templateUrl: './css-rule.component.html',
-    styleUrl: './css-rule.component.scss'
+  templateUrl: './css-rule.component.html',
+  styleUrl: './css-rule.component.scss'
 })
 export class CssRuleComponent implements OnInit {
   @Input() selectors: string[] = [];
