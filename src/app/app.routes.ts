681675a203ca369e2642918b7d443740eca299e5
PRBDUDE <programmerroadblocks@gmail.com>
2025-02-22 08:04:22 -0500

    fix routing issue

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {Routes} from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/color-palette',
+    redirectTo: '/demo/color-palette',
     pathMatch: 'full'
   },
   {
