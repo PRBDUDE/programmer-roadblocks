3faf81ab6e6dcae72ef61ab618e205c101841c5e
PRBDUDE <programmerroadblocks@gmail.com>
2025-02-26 07:02:29 -0500

    convert to FormBuilder

--- src/app/features/reactive-forms/reactive-entry-form/reactive-entry-form.component.ts
+++ src/app/features/reactive-forms/reactive-entry-form/reactive-entry-form.component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {DebugOutputCardComponent} from "@utility/debug-output-card.component";
 import {isDebugMode} from "@utility/is-debug-mode";
 
@@ -13,29 +13,34 @@ import {isDebugMode} from "@utility/is-debug-mode";
   templateUrl: './reactive-entry-form.component.html',
   styleUrl: './reactive-entry-form.component.scss'
 })
-export class ReactiveEntryFormComponent {
+export class ReactiveEntryFormComponent implements OnInit {
   protected readonly isDebugMode = isDebugMode;
 
   readonly validStateCodesPattern= /AL|AK|AZ|AR|CA|CO|CT|DE|FL|GA|HI|ID|IL|IN|IA|KS|KY|LA|ME|MD|MA|MI|MN|MS|MO|MT|NE|NV|NH|NJ|NM|NY|NC|ND|OH|OK|OR|PA|RI|SC|SD|TN|TX|UT|VT|VA|WA|WV|WI|WY/;
   readonly zipCodePattern = /^\d{5}(-\d{4})?$/;
   readonly phonePattern = /^\(?(\d{3})\)?[-. ]?(\d{3})[-. ]?(\d{4})$/;
 
-  entryForm: FormGroup = new FormGroup({
-    name: new FormGroup({
-      first: new FormControl('', Validators.required),
-      last: new FormControl('', Validators.required)
-    }),
-    address: new FormGroup({
-      street: new FormControl('', Validators.required),
-      city: new FormControl('', Validators.required),
-      state: new FormControl('', [Validators.required, Validators.pattern(this.validStateCodesPattern)]),
-      zip: new FormControl('', [Validators.required, Validators.pattern(this.zipCodePattern)]),
-    }),
-    contact: new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      phone: new FormControl('', [Validators.required, Validators.pattern(this.phonePattern)]),
-    })
-  } as any);
+  fb = inject(FormBuilder);
+  entryForm!: FormGroup;
+
+  ngOnInit() {
+    this.entryForm = this.fb.group({
+      name: this.fb.group({
+        first: ['', Validators.required],
+        last: ['', Validators.required]
+      }),
+      address: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', [Validators.required, Validators.pattern(this.validStateCodesPattern)]],
+        zip: ['', [Validators.required, Validators.pattern(this.zipCodePattern)]],
+      }),
+      contact: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        phone: ['', [Validators.required, Validators.pattern(this.phonePattern)]],
+      })
+    } as any);
+  }
 
   onSubmit() {
     console.log(this.entryForm.value);
