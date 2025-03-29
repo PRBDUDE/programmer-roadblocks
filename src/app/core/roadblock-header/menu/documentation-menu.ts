8973eb2880613d8ff5fbab2dcd4875ecda68a8d5
PRBDUDE <programmerroadblocks@gmail.com>
2025-03-28 20:28:47 -0400

    refactor menu items

--- src/app/core/roadblock-header/menu/documentation-menu.ts
+++ src/app/core/roadblock-header/menu/documentation-menu.ts
@@ -1,98 +1,24 @@
+import {documentationPrimengMenu} from "@rootComponents/menu/documentation-primeng-menu";
+import {documentationGettingStartedMenu} from "@rootComponents/menu/documentation-getting-started-menu";
+import {documentationTypescriptMenu} from "@rootComponents/menu/documentation-typescript-menu";
+import {documentationNodeMenu} from "@rootComponents/menu/documentation-node-menu";
+import {documentationReactiveFormsMenu} from "@rootComponents/menu/documentation-reactive-forms-menu";
+
 export function documentationMenu() {
     return {
         label: 'Documentation',
         root: true,
         items: [
             [
-                {
-                    label: 'PrimeNG',
-                    items: [
-                        {
-                            label: 'PrimeNG',
-                            subtext: 'PrimeNG official documentation',
-                            url: 'https://primeng.org/',
-                            target: '_blank'
-                        },
-                    ]
-                },
-                {
-                    label: 'Getting Started',
-                    items: [
-                        {
-                            label: 'Installation',
-                            subtext: 'PrimeNG Installation guide',
-                            url: 'https://primeng.org/installation',
-                            target: '_blank'
-                        },
-                        {
-                            label: 'Configuration',
-                            subtext: 'PrimeNG Configuration guide',
-                            url: 'https://primeng.org/configuration',
-                            target: '_blank'
-                        },
-                        {
-                            label: 'Playground',
-                            subtext: 'PrimeNG Playground',
-                            url: 'https://primeng.org/playground',
-                            target: '_blank'
-                        },
-                        {
-                            label: 'Theming',
-                            subtext: 'PrimeNG Theming',
-                            url: 'https://primeng.org/theming',
-                            target: '_blank'
-                        }
-                    ]
-                }
+                documentationPrimengMenu(),
+                documentationGettingStartedMenu()
             ],
             [
-                {
-                    label: 'Typescript',
-                    items: [
-                        {
-                            label: 'Language Reference',
-                            subtext: 'TypeScript official documentation',
-                            url: 'https://www.typescriptlang.org/docs/',
-                            target: '_blank'
-                        },
-                        {
-                            label: 'Tutorial',
-                            subtext: 'TypeScript Tutorial',
-                            url: 'https://www.w3schools.com/typescript/index.php',
-                            target: '_blank'
-                        }
-                    ]
-                },
-                {
-                    label: 'Node',
-                    items: [
-                        {
-                            label: 'Documentation',
-                            subtext: 'Node.js official documentation',
-                            url: 'https://nodejs.org/docs/latest/api/',
-                            target: '_blank'
-                        },
-                        {
-                            label: 'Tutorial',
-                            subtext: 'Angular Tutorial',
-                            url: 'https://www.w3schools.com/angular/',
-                            target: '_blank'
-                        }
-                    ]
-                }
+                documentationTypescriptMenu(),
+                documentationNodeMenu()
             ],
             [
-                {
-                    label: 'Reactive Forms',
-                    items: [
-                        {
-                            label: 'Reactive Forms',
-                            subtext: 'Angular Reactive Forms guide',
-                            url: 'https://angular.dev/guide/forms/reactive-forms',
-                            target: '_blank'
-                        }
-                    ]
-                }
+                documentationReactiveFormsMenu()
             ]
         ]
     }
---
+++ src/app/core/roadblock-header/menu/documentation-node-menu.ts
@@ -0,0 +1,19 @@
+export function documentationNodeMenu() {
+  return {
+    label: 'Node',
+    items: [
+      {
+        label: 'Documentation',
+        subtext: 'Node.js official documentation',
+        url: 'https://nodejs.org/docs/latest/api/',
+        target: '_blank'
+      },
+      {
+        label: 'Tutorial',
+        subtext: 'Angular Tutorial',
+        url: 'https://www.w3schools.com/angular/',
+        target: '_blank'
+      }
+    ]
+  }
+}
