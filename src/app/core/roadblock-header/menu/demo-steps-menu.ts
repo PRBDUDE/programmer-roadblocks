eb7691f150b2892cb48ab95b17b9137b7b042b8d
PRBDUDE <programmerroadblocks@gmail.com>
2025-04-11 07:56:51 -0400

    add steps to demo menu add stepper to demo menu

---
+++ src/app/core/roadblock-header/menu/demo-steps-menu.ts
@@ -0,0 +1,12 @@
+export function demoStepsMenu() {
+  return {
+    label: 'Steps',
+    items: [
+      {
+        label: 'Demo Steps',
+        subtext: 'Demo Steps',
+        route: '/demo/steps',
+      }
+    ]
+  }
+}
