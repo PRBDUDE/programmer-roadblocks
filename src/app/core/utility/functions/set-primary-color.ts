b5453cd69b1a23c41021a8708d6e85a6fba6116a
PRBDUDE <programmerroadblocks@gmail.com>
2025-04-17 10:31:33 -0400

    add function to set primary color add function to set surface color

---
+++ src/app/core/utility/functions/set-primary-color.ts
@@ -0,0 +1,21 @@
+import {updatePrimaryPalette} from "@primeng/themes";
+
+/**
+ * Sets the primary color of the application.
+ * @param color The color to set as primary.
+ */
+export function setPrimaryColor(color: string): void {
+  updatePrimaryPalette({
+    50: '{' + color + '.50}',
+    100: '{' + color + '.100}',
+    200: '{' + color + '.200}',
+    300: '{' + color + '.300}',
+    400: '{' + color + '.400}',
+    500: '{' + color + '.500}',
+    600: '{' + color + '.600}',
+    700: '{' + color + '.700}',
+    800: '{' + color + '.800}',
+    900: '{' + color + '.900}',
+    950: '{' + color + '.900}'
+  })
+}
