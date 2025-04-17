5800a4b6e2add94512ca8ee7eb22c80f1860b474
PRBDUDE <programmerroadblocks@gmail.com>
2025-04-17 10:29:29 -0400

    update color palette

--- src/app/features/demos/styling/color-palette/color-palette.component.ts
+++ src/app/features/demos/styling/color-palette/color-palette.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, DoCheck, OnInit, signal} from '@angular/core';
 import {$dt} from "@primeng/themes";
 import {PaletteGeneratorComponent} from "./palette-generator/palette-generator.component";
+import {isDarkMode} from "@utility/is-dark-mode";
 
 @Component({
   selector: 'prb-color-palette',
@@ -10,27 +11,40 @@ import {PaletteGeneratorComponent} from "./palette-generator/palette-generator.c
   templateUrl: './color-palette.component.html',
   styleUrl: './color-palette.component.scss'
 })
-export class ColorPaletteComponent {
-  primaryColor = $dt('primary.500').value;
-  emeraldColor = $dt('emerald.500').value;
-  greenColor = $dt('green.500').value;
-  limeColor = $dt('lime.500').value;
-  redColor = $dt('red.500').value;
-  orangeColor = $dt('orange.500').value;
-  amberColor = $dt('amber.500').value;
-  yellowColor = $dt('yellow.500').value;
-  tealColor = $dt('teal.500').value;
-  cyanColor = $dt('cyan.500').value;
-  skyColor = $dt('sky.500').value;
-  blueColor = $dt('blue.500').value;
-  indigoColor = $dt('indigo.500').value;
-  violetColor = $dt('violet.500').value;
-  purpleColor = $dt('purple.500').value;
-  fuchsiaColor = $dt('fuchsia.500').value;
-  pinkColor = $dt('pink.500').value;
-  slateColor = $dt('slate.500').value;
-  grayColor = $dt('gray.500').value;
-  zincColor = $dt('zinc.500').value;
-  neutralColor = $dt('neutral.500').value;
-  stoneColor = $dt('stone.500').value;
+export class ColorPaletteComponent implements DoCheck {
+  protected readonly isDarkMode = isDarkMode;
+
+  ngDoCheck() {
+    this.primaryColor.update(() => $dt('primary.500').value);
+    this.surfaceColor.update(() => $dt('surface.500').value);
+  }
+
+  primaryColor = signal<any>($dt('primary.500').value);
+  surfaceColor = signal<any>($dt('surface.500').value);
+  emeraldColor = signal<any>($dt('emerald.500').value);
+  greenColor = signal<any>($dt('green.500').value);
+  limeColor = signal<any>($dt('lime.500').value);
+  redColor = signal<any>($dt('red.500').value);
+  brownColor = signal<any>($dt('brown.500').value);
+  orangeColor = signal<any>($dt('orange.500').value);
+  amberColor = signal<any>($dt('amber.500').value);
+  yellowColor = signal<any>($dt('yellow.500').value);
+  tealColor = signal<any>($dt('teal.500').value);
+  cyanColor = signal<any>($dt('cyan.500').value);
+  skyColor = signal<any>($dt('sky.500').value);
+  blueColor = signal<any>($dt('blue.500').value);
+  indigoColor = signal<any>($dt('indigo.500').value);
+  violetColor = signal<any>($dt('violet.500').value);
+  purpleColor = signal<any>($dt('purple.500').value);
+  fuchsiaColor = signal<any>($dt('fuchsia.500').value);
+  pinkColor = signal<any>($dt('pink.500').value);
+  slateColor = signal<any>($dt('slate.500').value);
+  grayColor = signal<any>($dt('gray.500').value);
+  zincColor = signal<any>($dt('zinc.500').value);
+  neutralColor = signal<any>($dt('neutral.500').value);
+  stoneColor = signal<any>($dt('stone.500').value);
+  sohoColor = signal<any>($dt('soho.500').value);
+  vivaColor = signal<any>($dt('viva.500').value);
+  oceanColor = signal<any>($dt('ocean.500').value);
+  beachColor = signal<any>($dt('beach.500').value);
 }
