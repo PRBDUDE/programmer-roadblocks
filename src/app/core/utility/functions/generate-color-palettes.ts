93cb19f2c41707a42e6a44b15fcb61a2291a3ffc
PRBDUDE <programmerroadblocks@gmail.com>
2025-04-22 13:29:41 -0400

    remove unnecessary local variable

--- src/app/core/utility/functions/generate-color-palettes.ts
+++ src/app/core/utility/functions/generate-color-palettes.ts
@@ -4,8 +4,7 @@ import {palette} from "@primeng/themes";
 export function generateColorPalettes(colorDefinitions: ColorDefinition[]) {
   const result: Record<string, Record<string, string>> = {};
   colorDefinitions.forEach(colorDefinition => {
-    const colorPalette = palette(colorDefinition.baseColor);
-    result[colorDefinition.name.toLowerCase()] = colorPalette;
+    result[colorDefinition.name.toLowerCase()] = palette(colorDefinition.baseColor);
   });
   return result;
 }
