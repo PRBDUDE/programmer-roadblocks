fa7d84f55c63840bdf267011ed7d8e42edc27eba
PRBDUDE <programmerroadblocks@gmail.com>
2025-04-30 15:55:03 -0400

    adjust alignment

--- src/app/themes/aura-theme.ts
+++ src/app/themes/aura-theme.ts
@@ -10,12 +10,12 @@ import {TimelinePreset} from "./components/timeline-preset";
 import {TooltipPreset} from "./components/tooltip-preset";
 
 export const AuraPreset = definePreset(Aura, Primitive, Semantic, {
-    components: {
-        button: ButtonPreset,
-        datatable: DatatablePreset,
-        megamenu: MegamenuPreset,
-        message: MessagePreset,
-        timeline: TimelinePreset,
-        tooltip: TooltipPreset
-    }
+  components: {
+    button: ButtonPreset,
+    datatable: DatatablePreset,
+    megamenu: MegamenuPreset,
+    message: MessagePreset,
+    timeline: TimelinePreset,
+    tooltip: TooltipPreset
+  }
 });
