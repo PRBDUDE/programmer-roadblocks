51b635ecce98fa32a181b34018c475a3e0fb8bdb
PRBDUDE <programmerroadblocks@gmail.com>
2025-05-02 09:12:08 -0400

    adjust the transparency

--- src/app/themes/components/button-preset.ts
+++ src/app/themes/components/button-preset.ts
@@ -14,7 +14,7 @@ export const ButtonPreset = {
     },
     dark: {
       root: {
-        raisedShadow: "0 4px 2px -3px rgba(255, 255, 255, 0.2), 0 3px 4px 0 rgba(255, 255, 255, 0.14), 0 2px 7px 0 rgba(255, 255, 255, 0.12)",
+        raisedShadow: "0 4px 2px -3px rgba(255, 255, 255, 0.3), 0 3px 4px 0 rgba(255, 255, 255, 0.25), 0 2px 10px 0 rgba(255, 255, 255, 0.18)"
       }
     }
   }
