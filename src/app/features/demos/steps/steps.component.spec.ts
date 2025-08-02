f8df57f812004dac631920f4f9ebe84ca9b1e24b
PRBDUDE <programmerroadblocks@gmail.com>
2025-08-02 08:23:33 -0400

    add tests for next and previous

--- src/app/features/demos/steps/steps.component.spec.ts
+++ src/app/features/demos/steps/steps.component.spec.ts
@@ -2,6 +2,7 @@ import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {StepsComponent} from './steps.component';
 import {ActivatedRoute, convertToParamMap} from "@angular/router";
+import {By} from "@angular/platform-browser";
 
 describe('StepsComponent', () => {
   let component: StepsComponent;
@@ -28,10 +29,36 @@ describe('StepsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StepsComponent);
     component = fixture.componentInstance;
+    component.activeIndex = 0;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    const nextButton = fixture.debugElement.query(By.css('.prb-next-button p-button'));
+    expect(nextButton).toBeTruthy();
+    const previousButton = fixture.debugElement.query(By.css('.prb-previous-button p-button'));
+    expect(previousButton).toBeFalsy();
+  });
+
+  it('should move to next step', () => {
+    expect(component.activeIndex).toBe(0);
+    component.next();
+    expect(component.activeIndex).toBe(1);
+  });
+
+  it('should move to previous step', () => {
+    component.activeIndex = 1;
+    fixture.detectChanges();
+    expect(component.activeIndex).toBe(1);
+    component.prev();
+    expect(component.activeIndex).toBe(0);
+  })
+
+  it('should hide next button on last step', () => {
+    component.activeIndex = 3;
+    fixture.detectChanges();
+    const nextButton = fixture.debugElement.query(By.css('.prb-next-button p-button'));
+    expect(nextButton).toBeFalsy();
   });
 });
