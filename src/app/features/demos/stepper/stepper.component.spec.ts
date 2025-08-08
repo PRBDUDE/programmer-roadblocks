655a4144554efeeaa2c022ab80a9e45faf2640d9
PRBDUDE <programmerroadblocks@gmail.com>
2025-08-08 14:18:49 -0400

    add tests for next and previous

--- src/app/features/demos/stepper/stepper.component.spec.ts
+++ src/app/features/demos/stepper/stepper.component.spec.ts
@@ -2,6 +2,19 @@ import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {StepperComponent} from './stepper.component';
 import {provideAnimations} from "@angular/platform-browser/animations";
+import {By} from "@angular/platform-browser";
+
+function clickNextButton(fixture: ComponentFixture<StepperComponent>) {
+  const nextButtonEl = fixture.debugElement.query(By.css('.prb-next-button button'));
+  expect(nextButtonEl).toBeTruthy();
+  nextButtonEl.nativeElement.click();
+  fixture.detectChanges();
+}
+
+function getStepNumber(fixture: ComponentFixture<StepperComponent>): string {
+  const stepNumberEl = fixture.debugElement.query(By.css('.p-step-active .p-step-number'));
+  return stepNumberEl.nativeElement.textContent.trim();
+}
 
 describe('StepperComponent', () => {
   let component: StepperComponent;
@@ -27,4 +40,84 @@ describe('StepperComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('when the first step is active', () => {
+    it('should have previous button falsy at initial state', () => {
+      const prevButton = fixture.debugElement.query(By.css('.prb-previous-button button'));
+      console.log('prevButton: ', prevButton);
+    })
+
+    it('should have next button truthy at initial state', () => {
+      const nextButtonEl = fixture.debugElement.query(By.css('.prb-next-button button'));
+      expect(nextButtonEl).toBeTruthy();
+    })
+
+    it('should have step number 1', () => {
+      const stepNumber = getStepNumber(fixture);
+      expect(stepNumber).toBe('1');
+    })
+
+    describe('when the second step is active', () => {
+      beforeEach(() => {
+        clickNextButton(fixture);
+      })
+
+      it('should have previous button truthy at initial state', () => {
+        const prevButton = fixture.debugElement.query(By.css('.prb-previous-button button'));
+        expect(prevButton).toBeTruthy();
+      })
+
+      it('should have next button truthy at initial state', () => {
+        const nextButtonEl = fixture.debugElement.query(By.css('.prb-next-button button'));
+        expect(nextButtonEl).toBeTruthy();
+      })
+
+      it('should have step number 2', () => {
+        const stepNumber = getStepNumber(fixture);
+        expect(stepNumber).toBe('2');
+      })
+
+      describe('when the third step is active', () => {
+        beforeEach(() => {
+          clickNextButton(fixture);
+        })
+
+        it('should have previous button truthy at initial state', () => {
+          const prevButton = fixture.debugElement.query(By.css('.prb-previous-button button'));
+          expect(prevButton).toBeTruthy();
+        })
+
+        it('should have next button truthy at initial state', () => {
+          const nextButtonEl = fixture.debugElement.query(By.css('.prb-next-button button'));
+          expect(nextButtonEl).toBeTruthy();
+        })
+
+        it('should have step number 3', () => {
+          const stepNumber = getStepNumber(fixture);
+          expect(stepNumber).toBe('3');
+        })
+
+        describe('when the fourth step is active', () => {
+          beforeEach(() => {
+            clickNextButton(fixture);
+          })
+
+          it('should have previous button truthy at initial state', () => {
+            const prevButton = fixture.debugElement.query(By.css('.prb-previous-button button'));
+            expect(prevButton).toBeTruthy();
+          })
+
+          it('should have next button falsy at initial state', () => {
+            const nextButtonEl = fixture.debugElement.query(By.css('.prb-next-button button'));
+            expect(nextButtonEl).toBeFalsy();
+          })
+
+          it('should have step number 4', () => {
+            const stepNumber = getStepNumber(fixture);
+            expect(stepNumber).toBe('4');
+          })
+        })
+      })
+    })
+  })
 });
