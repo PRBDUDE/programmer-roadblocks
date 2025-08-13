8047138339ce4d16a6d9b95ea58afaeef47dda7c
PRBDUDE <programmerroadblocks@gmail.com>
2025-08-13 08:32:44 -0400

    fix compiler error caused by invalid type

--- src/app/features/demos/styling/color-palette/color-palette-generator/color-palette-generator.component.ts
+++ src/app/features/demos/styling/color-palette/color-palette-generator/color-palette-generator.component.ts
@@ -55,10 +55,10 @@ export class ColorPaletteGeneratorComponent {
     if (this.isSurfaceColor()) {
       this.colorValues = {
         0: "#ffffff",
-        ...palette(this.baseColor)
+        ...palette(this.baseColor) as PaletteShades,
       };
     } else {
-      this.colorValues = palette(this.baseColor);
+      this.colorValues = palette(this.baseColor) as PaletteShades;
     }
     if (this.colorValues) {
       this.shades = Object.keys(this.colorValues);
