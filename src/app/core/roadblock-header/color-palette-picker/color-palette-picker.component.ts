768bd593314f9ea8be53be55a5c613ff22d8e415
PRBDUDE <programmerroadblocks@gmail.com>
2025-08-20 15:20:36 -0400

    add the ability to turn Ripple on and off

--- src/app/core/roadblock-header/color-palette-picker/color-palette-picker.component.ts
+++ src/app/core/roadblock-header/color-palette-picker/color-palette-picker.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {Tooltip} from "primeng/tooltip";
 import {$dt} from "@primeuix/themes";
@@ -6,13 +6,16 @@ import {setPrimaryColor} from "@utility/set-primary-color";
 import {NgClass} from "@angular/common";
 import {setSurfaceColor} from "@utility/set-surface-color";
 import {isPrbMode, prbModes} from "@utility/is-prb-mode";
+import {ToggleSwitch} from "primeng/toggleswitch";
+import {PrimeNG} from "primeng/config";
 
 @Component({
   selector: 'prb-color-palette',
   imports: [
     FormsModule,
     Tooltip,
-    NgClass
+    NgClass,
+    ToggleSwitch
   ],
   templateUrl: './color-palette-picker.component.html',
   styleUrl: './color-palette-picker.component.scss'
@@ -22,6 +25,8 @@ export class ColorPalettePickerComponent {
   protected readonly setSurfaceColor = setSurfaceColor;
   protected readonly prbModes = prbModes;
   protected readonly isMode = isPrbMode;
+
+  primeng = inject(PrimeNG);
 
   private getColorValue(color: string) {
     let colorValue: any;
@@ -55,5 +60,13 @@ export class ColorPalettePickerComponent {
       surfaceValue = surfaceValue.light?.value;
     }
     return colorValue === surfaceValue;
+  }
+
+  setRippleEffect(rippleState: boolean) {
+    this.primeng.ripple.set(rippleState);
+  }
+
+  isRippleEffect() {
+    return this.primeng.ripple();
   }
 }
