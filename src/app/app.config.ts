4076ad5d8e97c1b0984ac317aaa5a9c107aff94f
PRBDUDE <programmerroadblocks@gmail.com>
2025-09-03 15:14:39 -0400

    update packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,14 @@ import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
+import {provideAnimations} from "@angular/platform-browser/animations";
 import {providePrimeNG} from "primeng/config";
 import {AuraPreset} from "./themes/aura/theme";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimationsAsync(),
+    provideAnimations(),
     providePrimeNG({
       theme: {
         preset: AuraPreset,
