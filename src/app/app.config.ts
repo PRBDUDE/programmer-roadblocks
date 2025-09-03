371f5f2d138a8ed6620155064bfe34f3c2297a55
PRBDUDE <programmerroadblocks@gmail.com>
2025-09-03 11:00:37 -0400

    add provideHttpClient replace provideAnimationsAsync with provideAnimations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,16 @@ import {ApplicationConfig} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideAnimationsAsync} from "@angular/platform-browser/animations/async";
 import {providePrimeNG} from "primeng/config";
+import {provideAnimations} from "@angular/platform-browser/animations";
 import {AuraPreset} from "./themes/aura/theme";
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimationsAsync(),
+    provideAnimations(),
+    provideHttpClient(),
     providePrimeNG({
       theme: {
         preset: AuraPreset,
