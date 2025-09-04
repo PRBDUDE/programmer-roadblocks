94a14525b1a4c340032bb810f6399ad154485864
PRBDUDE <programmerroadblocks@gmail.com>
2025-09-04 08:34:45 -0400

    fix failing test

--- src/app/features/demos/accordion/accordion.component.spec.ts
+++ src/app/features/demos/accordion/accordion.component.spec.ts
@@ -1,17 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { AccordionComponent } from './accordion.component';
+import {AccordionComponent} from './accordion.component';
+import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClientTesting} from "@angular/common/http/testing";
+import {AccordionModule} from "primeng/accordion";
+import {AccordionOptionsComponent} from "./accordion-options/accordion-options.component";
+import {JobHeaderComponent} from "./job-header/job-header.component";
+import {JobStatusComponent} from "./job-status/job-status.component";
+import {JobDateComponent} from "./job-date/job-date.component";
 
 describe('AccordionComponent', () => {
   let component: AccordionComponent;
   let fixture: ComponentFixture<AccordionComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AccordionComponent]
-    })
-    .compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AccordionModule,
+        AccordionOptionsComponent,
+        JobHeaderComponent,
+        JobStatusComponent,
+        JobDateComponent
+      ],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ],
+    }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(AccordionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
