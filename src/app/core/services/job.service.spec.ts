9631c585c7536716601c6a4a8776740fe4c60dc6
PRBDUDE <programmerroadblocks@gmail.com>
2025-09-04 08:56:10 -0400

    fix failing test

--- src/app/core/services/job.service.spec.ts
+++ src/app/core/services/job.service.spec.ts
@@ -1,12 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed, waitForAsync} from '@angular/core/testing';
 
-import { JobService } from './job.service';
+import {JobService} from './job.service';
+import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClientTesting} from "@angular/common/http/testing";
 
 describe('JobService', () => {
   let service: JobService;
 
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    }).compileComponents();
+  }));
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
     service = TestBed.inject(JobService);
   });
 
