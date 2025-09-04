3da5a3da507b70b24ded170a17606de87a921d1c
PRBDUDE <programmerroadblocks@gmail.com>
2025-09-04 08:44:15 -0400

    fix failing test

--- src/app/features/demos/styling/palette-generator/palette-generator.component.spec.ts
+++ src/app/features/demos/styling/palette-generator/palette-generator.component.spec.ts
@@ -1,6 +1,13 @@
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {PaletteGeneratorComponent} from './palette-generator.component';
+import {CommonModule} from "@angular/common";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {TableModule} from "primeng/table";
+import {CardModule} from "primeng/card";
+import {InputNumberModule} from "primeng/inputnumber";
 
 describe('PaletteGeneratorComponent', () => {
   let component: PaletteGeneratorComponent;
@@ -9,7 +16,15 @@ describe('PaletteGeneratorComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
-        PaletteGeneratorComponent
+        PaletteGeneratorComponent,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        InputTextModule,
+        ButtonModule,
+        TableModule,
+        CardModule,
+        InputNumberModule
       ]
     }).compileComponents();
   }));
