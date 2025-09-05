63a61cd353a0df0f78d9479869d95ca12ed51f7a
PRBDUDE <programmerroadblocks@gmail.com>
2025-09-05 06:56:40 -0400

    add setPrimaryPalette add setSurfacePalette

--- src/app/core/roadblock-header/color-palette-picker/color-palette-picker.component.ts
+++ src/app/core/roadblock-header/color-palette-picker/color-palette-picker.component.ts
@@ -1,13 +1,15 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {Tooltip} from "primeng/tooltip";
-import {$dt} from "@primeuix/themes";
-import {setPrimaryColor} from "@utility/set-primary-color";
+import {$dt, updatePrimaryPalette, updateSurfacePalette} from "@primeuix/themes";
 import {NgClass} from "@angular/common";
-import {setSurfaceColor} from "@utility/set-surface-color";
 import {isPrbMode, prbModes} from "@utility/is-prb-mode";
 import {ToggleSwitch} from "primeng/toggleswitch";
 import {PrimeNG} from "primeng/config";
+import {ProfileService} from "@services/profile.service";
+import {UserProfile} from "@model/user-profile";
+import {setPrimaryColor} from "@utility/set-primary-color";
+import {setSurfaceColor} from "@utility/set-surface-color";
 
 @Component({
   selector: 'prb-color-palette',
@@ -20,13 +22,32 @@ import {PrimeNG} from "primeng/config";
   templateUrl: './color-palette-picker.component.html',
   styleUrl: './color-palette-picker.component.scss'
 })
-export class ColorPalettePickerComponent {
-  protected readonly setPrimaryColor = setPrimaryColor;
-  protected readonly setSurfaceColor = setSurfaceColor;
+export class ColorPalettePickerComponent implements OnInit {
   protected readonly prbModes = prbModes;
   protected readonly isMode = isPrbMode;
 
+  profile: UserProfile = {
+    primary: 'sky',
+    surface: 'neutral',
+    ripple: 'true',
+    footer: 'sticky',
+    debug: 'true',
+    theme: 'dark'
+  };
+
   primeng = inject(PrimeNG);
+
+  constructor(private profileService: ProfileService) {}
+
+  ngOnInit() {
+    this.profileService.getProfile().subscribe(profile => {
+      this.profile.primary = profile.primary;
+      this.profile.surface = profile.surface;
+      this.profile.ripple = profile.ripple;
+      console.log('Loaded profile: ',
+        this.profile)
+    })
+  }
 
   private getColorValue(color: string) {
     let colorValue: any;
@@ -62,8 +83,29 @@ export class ColorPalettePickerComponent {
     return colorValue === surfaceValue;
   }
 
+  updatePalette() {
+    this.profileService.setPalette(this.profile.primary, this.profile.surface, this.profile.ripple === 'true');
+    console.log('Updated palette to: ',
+      this.profile
+    );
+  }
+
+  setPrimaryPalette(color: string): void {
+    setPrimaryColor(color);
+    this.profile.primary = color;
+    this.updatePalette();
+  }
+
+  setSurfacePalette(color: string) {
+    setSurfaceColor(color);
+    this.profile.surface = color;
+    this.updatePalette();
+  }
+
   setRippleEffect(rippleState: boolean) {
     this.primeng.ripple.set(rippleState);
+    this.profile.ripple = rippleState.toString();
+    this.updatePalette();
   }
 
   isRippleEffect() {
