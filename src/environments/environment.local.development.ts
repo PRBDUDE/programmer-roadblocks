47cfa381248e84d0b6a1dfc27454f3dcec8c65f4
PRBDUDE <programmerroadblocks@gmail.com>
2025-09-05 06:17:38 -0400

    use proxy

--- src/environments/environment.local.development.ts
+++ src/environments/environment.local.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   envPrefix: 'dev',
-  userProfileUrl: "http://localhost:3000/profile",
-  jobStatusUrl: "http://localhost:3000/jobs"
+  userProfileUrl: "/api/profile",
+  jobStatusUrl: "/api/jobs"
 };
