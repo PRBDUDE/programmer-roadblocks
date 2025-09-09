cf5e70057f784aa764da1a1157c87d821fb6c2fa
PRBDUDE <programmerroadblocks@gmail.com>
2025-09-09 09:57:03 -0400

    remove constructor move function calls down into ts file

--- src/app/core/roadblock-header/roadblock-header.component.ts
+++ src/app/core/roadblock-header/roadblock-header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {MegaMenuModule} from "primeng/megamenu";
 import {MegaMenuItem} from "primeng/api";
 import {RouterLink} from "@angular/router";
@@ -10,7 +10,8 @@ import {reactiveFormsMenu} from "@rootComponents/menu/reactive-forms-menu";
 import {demosMenu} from "@rootComponents/menu/demos-menu";
 import {documentationMenu} from "@rootComponents/menu/documentation-menu";
 import {ColorPalettePickerComponent} from "@rootComponents/color-palette-picker/color-palette-picker.component";
-import {isPrbMode, prbModes, setDarkTheme, setDebugMode, toggleMode} from "@utility/prb-mode";
+import {isPrbMode, prbModes, setDarkTheme, setDebugMode, setFixedFooter, toggleMode} from "@utility/prb-mode";
+import {ProfileService} from "@services/profile.service";
 
 @Component({
   selector: 'prb-roadblock-header',
@@ -26,13 +27,8 @@ import {isPrbMode, prbModes, setDarkTheme, setDebugMode, toggleMode} from "@util
   styleUrl: './roadblock-header.component.scss'
 })
 export class RoadblockHeaderComponent implements OnInit {
-  protected readonly isProduction = isProduction;
-  protected readonly isMode = isPrbMode;
-  protected readonly prbModes = prbModes;
-  protected readonly toggleMode = toggleMode;
-  protected readonly setDebugMode = setDebugMode;
-  protected readonly setDarkTheme = setDarkTheme;
 
+  private profileService = inject(ProfileService);
   menuItems: MegaMenuItem[] | undefined;
 
   ngOnInit() {
@@ -41,5 +37,61 @@ export class RoadblockHeaderComponent implements OnInit {
       demosMenu(),
       documentationMenu(),
     ]
+  }
+
+  isProductionEnvironment(): boolean {
+    return isProduction();
+  }
+
+  toggleColorPicker() {
+    toggleMode(prbModes.colorPicker);
+    console.log('Color picker clicked');
+  }
+
+  isDarkTheme(): boolean {
+    return isPrbMode(prbModes.dark);
+  }
+
+  darkTheme() {
+    this.profileService.setDarkThemeProfile(true).subscribe(
+      result => console.log('Dark theme set to: ', result)
+    );
+    setDarkTheme(true);
+  }
+
+  lightTheme() {
+    this.profileService.setDarkThemeProfile(false).subscribe(
+      result => console.log('Light theme set to: ', result)
+    );
+    setDarkTheme(false);
+  }
+
+  isDebug(): boolean {
+    return isPrbMode(prbModes.debug);
+  }
+
+  setDebug(on: boolean) {
+    this.profileService.setDebugProfile(on).subscribe(
+      result => console.log('Debug mode set to: ', result)
+    );
+    setDebugMode(on);
+  }
+
+  isFixedFooter(): boolean {
+    return isPrbMode(prbModes.fixedFooter);
+  }
+
+  fixed() {
+    this.profileService.setFixedFooterProfile(true).subscribe(
+      result => console.log('Fixed footer set to: ', result)
+    );
+    setFixedFooter(true);
+  }
+
+  floatng() {
+    this.profileService.setFixedFooterProfile(false).subscribe(
+      result => console.log('Floating footer set to: ', result)
+    );
+    setFixedFooter(false);
   }
 }
