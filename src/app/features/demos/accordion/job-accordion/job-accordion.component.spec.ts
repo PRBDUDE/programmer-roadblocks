ede12081d15bf5e28442504627a7ff4d3335809f
PRBDUDE <programmerroadblocks@gmail.com>
2025-09-19 10:10:39 -0400

    fixed failing test

--- src/app/features/demos/accordion/job-accordion/job-accordion.component.spec.ts
+++ src/app/features/demos/accordion/job-accordion/job-accordion.component.spec.ts
@@ -1,17 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
-import { JobAccordionComponent } from './job-accordion.component';
+import {JobAccordionComponent} from './job-accordion.component';
+import {provideHttpClientTesting} from "@angular/common/http/testing";
 
 describe('JobAccordionComponent', () => {
   let component: JobAccordionComponent;
   let fixture: ComponentFixture<JobAccordionComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [JobAccordionComponent]
-    })
-    .compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        JobAccordionComponent
+      ],
+      providers: [
+        provideHttpClientTesting()
+      ]
+    }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(JobAccordionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
