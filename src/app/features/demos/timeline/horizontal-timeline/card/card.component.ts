ffc68fb6a570b3bbfb3ae4bd10afcd474a5fa6fa
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-21 13:23:38 -0500

    add card-header

--- src/app/features/demos/timeline/horizontal-timeline/card/card.component.ts
+++ src/app/features/demos/timeline/horizontal-timeline/card/card.component.ts
@@ -2,17 +2,19 @@ import {Component, input} from '@angular/core';
 import {Card} from "primeng/card";
 import {DatePipe} from "@angular/common";
 import {TimelineEventItem} from "../../module/timeline-event-item";
+import {CardHeaderComponent} from "../../card-header/card-header.component";
 
 @Component({
   selector: 'prb-card',
   imports: [
     Card,
-    DatePipe
+    DatePipe,
+    CardHeaderComponent
   ],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss',
     '../horizontal-timeline.component.scss']
 })
 export class CardComponent {
-  eventItem = input<TimelineEventItem>();
+  eventItem = input.required<TimelineEventItem>();
 }
--- src/app/features/demos/timeline/vertical-timeline/card/card.component.ts
+++ src/app/features/demos/timeline/vertical-timeline/card/card.component.ts
@@ -2,17 +2,19 @@ import {Component, input} from '@angular/core';
 import {Card} from "primeng/card";
 import {DatePipe} from "@angular/common";
 import {TimelineEventItem} from "../../module/timeline-event-item";
+import {CardHeaderComponent} from "../../card-header/card-header.component";
 
 @Component({
   selector: 'prb-card',
   imports: [
     DatePipe,
-    Card
+    Card,
+    CardHeaderComponent
   ],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss',
     '../vertical-timeline.component.scss']
 })
 export class CardComponent {
-  eventItem = input<TimelineEventItem>();
+  eventItem = input.required<TimelineEventItem>();
 }
