4336088870554a711cc89512e4281dae1ef51586
PRBDUDE <programmerroadblocks@gmail.com>
2026-01-25 20:50:11 -0500

    add vitest config

---
+++ src/test/vitest.setup.ts
@@ -0,0 +1,13 @@
+if (!window.matchMedia) {
+  window.matchMedia = (query: string): MediaQueryList =>
+    ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => undefined,
+      removeListener: () => undefined,
+      addEventListener: () => undefined,
+      removeEventListener: () => undefined,
+      dispatchEvent: () => false,
+    }) as MediaQueryList
+}
