8a4ea31cbc978c21dbd1b08fee10e1cd23d647c4
PRBDUDE <programmerroadblocks@gmail.com>
2026-02-19 16:34:20 -0500

    update import path to theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {providePrimeNG} from "primeng/config";
 import {provideAnimations} from "@angular/platform-browser/animations";
-import {AuraPreset} from "./themes/aura/theme";
+import {AuraPreset} from "@themes/aura/theme";
 import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
