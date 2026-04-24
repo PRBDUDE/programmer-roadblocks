b8c2aa5d2dfa24462417b26829302d80fa8a4548
PRBDUDE <programmerroadblocks@gmail.com>
2026-04-24 08:00:38 -0400

    add demo-datepicker component to routes

---
+++ src/app/core/roadblock-header/menu/demo-datepicker.ts
@@ -0,0 +1,12 @@
+export function demoDatepicker() {
+  return {
+    label: 'Datepicker',
+    items: [
+      {
+        label: 'Datepicker',
+        subtext: 'Demo Datepicker',
+        route: '/demo/datepicker',
+      }
+    ]
+  }
+}
